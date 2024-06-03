1f6bef4b68d3548cbdf935fa926e6a0162f64ba6
FXT-Spacer <openfxt.spacer@hotmail.com>
2024-06-03 18:14:15 +0000

    public

---
+++ assets/main.js
@@ -0,0 +1,38 @@
+document.getElementById("generation").addEventListener("click", function () {
+    const inputValue = document.getElementById("input").value;
+    const data = { message: inputValue };
+    const imgElement = document.getElementById("image");
+    const button = document.getElementById("generation");
+
+    imgElement.style.display = "none";
+    button.disabled = true;
+    button.textContent = "Creating...";
+
+    fetch('https://chatfxt.pythonanywhere.com/api/v2/image_response', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.text())
+        .then(link => {
+            if (link && link !== "") {
+                imgElement.src = link;
+                imgElement.style.display = "block";
+            } else {
+                imgElement.src = "/assets/blank.png";
+                imgElement.style.display = "block";
+            }
+            button.disabled = false;
+            button.textContent = "Generation";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            imgElement.src = "/assets/blank.png";
+            imgElement.style.display = "block";
+            button.disabled = false;
+            button.textContent = "Generation";
+        });
+});
+alert("Xin chào, đây là OpenFXT Image Generator, dịch vụ tạo ảnh theo yêu cầu sử dụng bất kỳ ngôn ngữ nào, hiện tại giao diện của website chỉ hỗ trợ trên desktop một cách mượt mà, bạn vui lòng sử dụng thiết bị laptop, PC để sử dụng dịch vụ một cách dễ dàng nhất. Dịch vụ này đã public API và source code trên mục API Docs và Github đi kèm, bạn vui lòng tham khảo thêm. Một vấn đề nữa là do hạn chế về kinh phí và nhân lực, dịch vụ sẽ không có uptime 100%, nếu trong quá trình sử dụng dịch vụ xảy ra lỗi, bạn vui lòng đợi và thử lại sau. Cảm ơn bạn đã quan tâm và sử dụng dịch vụ của OpenFXT!")
---
+++ assets/notification.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var notification = document.getElementById('notification');
+    notification.style.display = 'block';
+    notification.style.animation = 'slideUp 0.5s forwards';
+});
